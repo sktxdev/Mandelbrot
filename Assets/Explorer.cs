a304fe651f226e8dc83556b808031a7bdc132197
Sunil Khandwala <sktxdev@gmail.com>
2021-01-30 20:51:41 -0600

    Add movement control via WASD keys

--- Assets/Explorer.cs
+++ Assets/Explorer.cs
@@ -7,6 +7,8 @@ public class Explorer : MonoBehaviour
     public Material mat;
     public Vector2 pos;
     public float scale;
+    public float speed;
+
 
     public int counter = 0;
 
@@ -32,19 +34,39 @@ public class Explorer : MonoBehaviour
     void FixedUpdate()
     {
         HandleInput();
+        HandlePanning();
         UpdateShader();
-
     }
 
     private void HandleInput() {
         if (Input.GetKey(KeyCode.KeypadPlus))
             scale *= 0.99f;
-        
+
         if (Input.GetKey(KeyCode.KeypadMinus))
             scale *= 1.01f;
-
     }
 
+    private void HandlePanning()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            pos.x -= .01f * scale;
+        }
 
+        if (Input.GetKey(KeyCode.A))
+        {
+            pos.x += 0.01f * scale;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            pos.y -= .01f * scale;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            pos.y += 0.01f * scale;
+        }
+    }
 
 }
